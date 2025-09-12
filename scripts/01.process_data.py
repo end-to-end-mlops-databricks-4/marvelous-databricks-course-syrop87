8c3be8a998eed5846923d01f85786e7f421b42a9
Pawel Borys <pborys@gmail.com>
2025-09-12 17:49:38 +0200

    fixing dbx connection by adding user profile

--- scripts/01.process_data.py
+++ scripts/01.process_data.py
@@ -1,6 +1,7 @@
 import yaml
+import os
 from loguru import logger
-from pyspark.sql import SparkSession
+from databricks.connect import DatabricksSession
 
 from games_sales import PROJECT_DIR
 from games_sales.config import ProjectConfig
@@ -12,7 +13,7 @@ config = ProjectConfig.from_yaml(config_path=(PROJECT_DIR / "project_config.yml"
 logger.info("Configuration loaded:")
 logger.info(yaml.dump(config, default_flow_style=False))
 
-spark = SparkSession.builder.getOrCreate()
+spark = DatabricksSession.builder.profile(os.environ.get('PROFILE_NAME')).getOrCreate()
 
 # Load data
 data_loader = DataLoader(config)
@@ -28,8 +29,6 @@ train_df, test_df = data_processor.split_by_time(
     test_periods=2, 
     min_train_periods=int(config.preprocessing['min_years']*0.8))
 
-logger.info("Training set shape: %s", train_df.shape)
-logger.info("Test set shape: %s", test_df.shape)
 
 # Save to catalog
 data_processor.save_to_catalog(train_df, test_df)
